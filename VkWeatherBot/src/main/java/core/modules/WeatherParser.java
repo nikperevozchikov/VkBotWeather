2006f7a05e4e896342b44a2a424ba38957c44abd
nikperevozchikov <31481190+nikperevozchikov@users.noreply.github.com>
2020-09-15 09:43:48 +0400

    add project with Long Poll

---
+++ VkWeatherBot/src/main/java/core/modules/WeatherParser.java
@@ -0,0 +1,30 @@
+package core.modules;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+/**
+ * Парсер погоды
+ */
+public class WeatherParser {
+    private String city = "samara";
+    private Document doc;
+    public WeatherParser() throws IOException {
+        doc = Jsoup.connect(String.format("https://world-weather.ru/pogoda/russia/%s/",city)).get();
+    }
+    WeatherParser(String city) throws IOException {
+        this.city = city;
+        doc = Jsoup.connect(String.format("https://world-weather.ru/pogoda/russia/%s/",city)).get();
+    }
+
+
+    public String getWeatherTodayDescription() {
+        Elements elements = doc.select("span.dw-into");
+        return elements.text().split("Подробнее")[0];
+    }
+
+}
